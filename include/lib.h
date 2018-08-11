93dea4a9e5790a159e7be37e1179bdd71bf172c9
Mogball <jeffniu22@gmail.com>
2018-08-11 10:10:47 -0400

    Initial commit

---
+++ include/lib.h
@@ -0,0 +1,3 @@
+#pragma once
+
+extern int println(const char *str);
---
+++ lib/lib.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int println(const char *str) {
+    return s_println(str);
+}
+
+static s_println(const char *str) {
+    return s_println(str);
+}
