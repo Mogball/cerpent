d698ef307648a18f92e68926ee880c3b31991cd5
Mogball <jeffniu22@gmail.com>
2018-09-21 08:52:30 -0400

    removed symbol table

--- llvm/src/llvm-visitor.cpp
+++ llvm/src/llvm-visitor.cpp
@@ -20,15 +20,10 @@ bool InterpreterVisitor::TraverseType(QualType type) {
     return true;
 }
 
-#include "symboltable.hpp"
-
-static SymbolTable table;
-
 bool InterpreterVisitor::TraverseVarDecl(VarDecl *decl) {
     RecursiveASTVisitor<InterpreterVisitor>::TraverseVarDecl(decl);
     APValue *val = decl->evaluateValue();
     if (nullptr != val) {
-        table.addSymbol(decl->getIdentifier()->getName(), val);
     }
     return true;
 }
