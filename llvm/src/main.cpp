14ab4fe6c8cf7c467bd63624e280eaac57de9a2f
Mogball <jeffniu22@gmail.com>
2018-09-22 20:10:33 -0400

    can now run files

--- llvm/src/main.cpp
+++ llvm/src/main.cpp
@@ -1,33 +1,48 @@
 #include "interp.hpp"
 #include <iostream>
+#include <fstream>
 #include <cassert>
 
 using namespace std;
 
 template<typename stream_t>
-static error_t interp_loop(int argc, char *argv[], stream_t &s) {
+static error_t interp_loop(int argc, char *argv[], stream_t &s, bool cmd) {
     Interpreter interp(argc, argv);
     error_t err = OK;
-    cout << "Cerpent v1.0" << endl;
-    cout << "> ";
+    if (cmd) {
+        cout << "Cerpent v1.0" << endl;
+        cout << "> ";
+    }
     for (string line; getline(s, line);) {
         err = interp.processLine(move(line));
         assert(line.empty());
         if (OK != err) {
             printf("ERROR: 0x%04x\n", err);
         }
-        if (interp.isBufferEmpty()) {
-            cout << "> ";
-        } else {
-            cout << "  ";
-        }
-        for (int i = 0; i < interp.getIndent(); ++i) {
-            cout << "  ";
+        if (cmd) {
+            if (interp.isBufferEmpty()) {
+                cout << "> ";
+            } else {
+                cout << "  ";
+            }
+            for (int i = 0; i < interp.getIndent(); ++i) {
+                cout << "  ";
+            }
         }
     }
     return err;
 }
 
 int main(int argc, char *argv[]) {
-    return static_cast<int>(interp_loop(argc, argv, cin));
+    if (argc == 2) {
+        ifstream inFile;
+        inFile.open(argv[1], ios::in);
+        error_t ret = static_cast<int>(interp_loop(argc - 1, argv, inFile, false));
+        inFile.close();
+        return ret;
+    } else if (argc > 2) {
+        cout << "cerp [[script.cerp]]" << endl;
+        return -1;
+    }
+    return static_cast<int>(interp_loop(argc, argv, cin, true));
 }
